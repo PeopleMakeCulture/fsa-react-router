f372e0bc2c5ff95cfcd968c3754f922b0636c722
Jing Cao <jing.cao@Jings-MacBook-Pro.local>
2023-07-31 19:33:58 -0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 const App: React.FunctionComponent = () => {
@@ -9,7 +10,12 @@ const App: React.FunctionComponent = () => {
 <div id="container">
 <h1>Hello React Router!</h1>
 <div id="navbar">{/* navigation here */}</div>
-<div id="main-section">{/* routes here */}</div>
+<div id="main-section">
+  <Routes>
+    <Route path="/blue" element={<h1>Blue</h1>} />
+    <Route path="/red" element={<h1>Red</h1>} />   
+  </Routes>
+  </div>
 </div>
     </>
   )
