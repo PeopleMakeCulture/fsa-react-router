8ba71c3705fcb017893afa8ad27a17bb8118c11d
Jing Cao <jing.cao@Jings-MacBook-Pro.local>
2023-07-31 20:01:23 -0400

    add home tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Red  from './components/Red'
 import Blue from './components/Blue'
+import Home from './components/Home'
 const App: React.FunctionComponent = () => {
 
   return (
@@ -11,11 +12,13 @@ const App: React.FunctionComponent = () => {
 <div id="navbar">
   <Link to="/blue">Blue</Link>
   <Link to="/red">Red</Link> 
+  <Link to="/">Home</Link>
 </div>
 <div id="main-section">
   <Routes>
     <Route path="/blue" element={<Blue/>} />
-    <Route path="/red" element={<Red/>} />   
+    <Route path="/red" element={<Red/>} />  
+    <Route path="/" element={<Home/>} />   
   </Routes>
   </div>
 </div>
